27141076566a55f3743778e7a29b1ce7500e84a6
aibrohim <ibrohim7840@gmail.com>
2021-10-07 17:51:05 +0500

    Added page loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import { Provider } from 'next-auth/client';
+import NextNProgress from 'nextjs-progressbar';
 import '../src/assets/scss/main.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <NextNProgress
+        color="#1D6B5C"
+        startPosition={0}
+        stopDelayMs={0}
+        height={3}
+      />
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
